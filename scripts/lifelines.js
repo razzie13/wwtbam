09101c9fdc0eff608689a1a7f30187a6f1ecf004
razzie13 <gregrasmussen@gmail.com>
2023-10-25 21:23:39 -0400

    Initial WWTBAM JS Commit

---
+++ scripts/lifelines.js
@@ -0,0 +1,18 @@
+const lifelines =
+{
+    lifeline_one: {
+       name: "50/50",
+       position: 0,
+       used: false 
+    },
+    lifeline_two: {
+        name: "Ask The Audience",
+        position: 1,
+        used: false 
+    },
+    lifeline_three: {
+        name: "Phone A Friend",
+        position: 2,
+        used: false 
+    }
+}
---
+++ scripts/question_template.js
@@ -0,0 +1,13 @@
+const questions =
+{
+    question: {
+       question_list_value: "",
+       question_text: "",
+       question_value_a: "",
+       question_value_b: "",
+       question_value_c: "",
+       question_value_d: "",
+       question_correct_answer_value: "",
+       used: false 
+    }
+}
